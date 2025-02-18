8ad80d2fa4edfca7068388dc476ba09a5662e012
Mchikhi59 <miloud.celine@gmail.com>
2025-02-17 19:06:47 -0500

    first commit

---
+++ src/test/java/first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("this is the project number 1");
+    }
+}
+//donne with version 1
