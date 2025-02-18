73d2fb8e3cff39d4c1464ab4b3f6c4e6e71fe29f
Mchikhi59 <miloud.celine@gmail.com>
2025-02-17 19:27:30 -0500

    second commit

--- src/test/java/first.java
+++ src/test/java/first.java
@@ -2,6 +2,8 @@ public class first {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is the project number 1");
+        System.out.println("this is the project number 1");
+        System.out.println("this is the project number 1");
     }
 }
 //donne with version 1
